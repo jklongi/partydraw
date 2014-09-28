f6359bddc945ca48b6eadf7baf9ed97019f2a954
jklongi <joonas.longi@helsinki.fi>
2014-09-28 15:29:14 +0300

    views added

---
+++ src/com/example/partydrawandroid/PrepareActivity.java
@@ -0,0 +1,54 @@
+package com.example.partydrawandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class PrepareActivity extends Activity {
+	
+	Button ready;
+	EditText playername;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+	    getActionBar().hide();
+		setContentView(R.layout.activity_prepare);
+		
+		
+		ready = (Button) findViewById(R.id.readyButton);
+		
+		
+	}
+	
+	public void startDraw(View view){
+		Intent intent = new Intent(this, MainActivity.class);	
+    	startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.prepare, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
